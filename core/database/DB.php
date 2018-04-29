e332bb384f5abca8003ef78b1b91b22b35c0a38c
Deivid Staehr <deividstaehr@hotmail.com>
2018-04-28 21:05:02 -0300

    update

--- core/database/DB.php
+++ core/database/DB.php
@@ -6,31 +6,29 @@ use core\database\PDOFactory;
 
 class DB
 {
-    private static $pdo;
+    private static $conn;
     private $table;
 
     public function __construct($table) {
         $this->table = $table;
     }
 
-    private function open()
-    {
-
-    }
-
     public function begin()
     {
-
+		self::$conn = PDOFactory::make($database);
+		self::$conn->beginTransaction();
     }
 
     public function commit()
     {
-
+		self::$conn->commit();
+		self::$conn = null;
     }
 
     public function rollback()
     {
-
+        self::$conn->rollback();
+        self::$conn = null;
     }
 
     public function rawQuery($query)
@@ -61,5 +59,10 @@ class DB
     public function select()
     {
         
+    }
+
+    public function get()
+    {
+        return self::$conn;
     }
 }
--- core/database/PDOFactory.php
+++ core/database/PDOFactory.php
@@ -10,11 +10,44 @@ class PDOFactory
 
     public static function make()
     {
-
+        $db = self::parseDatabaseIniFile($dbinfo = 'database');
+   
+        $user  = isset($db['user']) ? $db['user'] : NULL;
+        $pass  = isset($db['pass']) ? $db['pass'] : NULL;
+        $name  = isset($db['name']) ? $db['name'] : NULL;
+        $host  = isset($db['host']) ? $db['host'] : NULL;
+        $type  = isset($db['type']) ? $db['type'] : NULL;
+        $port  = isset($db['port']) ? $db['port'] : NULL;
+        
+        switch ($type)
+        {
+            case 'pgsql':
+                $port = $port ? $port : '5432';
+                $pdo = new \PDO("pgsql:dbname={$name};user={$user}; password={$pass};host=$host;port={$port}");
+                break;
+            case 'mysql':
+                $port = $port ? $port : '3306';
+                $pdo = new \PDO("mysql:host={$host};port={$port};dbname={$name}", $user, $pass);
+                break;
+            default:
+                throw new \Exception("Driver não encrontrado ('{$type}')");
+                break;
+        }
+        
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+        
+        return $pdo;
     }
 
-    private function parseIniFile()
+    private function parseDatabaseIniFile()
     {
+        $iniFile = Path::find('root')."core/config/{$database}.ini";
         
+        if (file_exists($iniFile)) {
+            return parse_ini_file($iniFile);
+        } else {
+            throw new \Exception("Arquivo não encrontrado ('{$database}.ini')");
+        }
     }
 }
